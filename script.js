2c151e4f0a1ca2ee66650acb7e86fe4718645bc2
hiroki0104 <hiroki010419@outlook.jp>
2020-06-09 04:25:07 +0900

    shotting_add

--- script.js
+++ script.js
@@ -1,7 +1,33 @@
-// const gameListEl = document.getElementById('geme-list');
-// const cloudEl = document.getElementById('cloud-link');
+const gameListEl = document.getElementById('game-list');
+const cloudEl = document.getElementById('cloud-link');
 
-// const gameList = [
-//   {title: "シューティングゲーム",
-//     url: ""}
-// ]
+const gameList = [
+  { title: 'シューティングゲーム', url: './js_game_practice/index.html' },
+];
+
+function createGameList() {
+  gameList.forEach((game) => {
+    const gameList = document.createElement('li');
+    gameList.innerHTML = `
+    <span>${game.title}</span><a href=${game.url}><i class="fas fa-arrow-right"></i></a>
+    `;
+
+    gameListEl.appendChild(gameList);
+  });
+}
+
+function createLink() {
+  gameList.forEach((game) => {
+    const linkList = document.createElement('a');
+    linkList.classList.add('cloud');
+    linkList.setAttribute('href', `${game.url}`);
+    linkList.innerHTML = `
+      <img src="./img/cloud.png" alt="雲" />
+      <span>${game.title}で遊ぶ</span>
+    `;
+    cloudEl.appendChild(linkList);
+  });
+}
+
+createGameList();
+createLink();
